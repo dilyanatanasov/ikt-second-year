3c59f7e80124616eb321118df1728ad2566fdc4e
Dilyan <dilyan.atanasov@scavasoft.com>
2021-03-25 08:09:00 +0200

    progress

--- views/site/movies.php
+++ views/site/movies.php
@@ -1,10 +1,18 @@
 <?php
 require_once "navigation.php";
 
-?>
+if (!empty($data)) {
+    if (!empty($_GET["action"])) {
+        switch ($_GET["action"]) {
+            case "listAll":
+                require_once "html/movieList.php";
+                break;
+            default:
+                require_once "html/movieList.php";
+                break;
+        }
+    }
+}
 
-
-
-<?php
 require_once "footer.php";
 ?>
