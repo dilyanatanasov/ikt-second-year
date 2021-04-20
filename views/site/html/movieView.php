237a1372b3d89caa8e97aa3e3834f3527fa17736
Dilyan <dilyan.atanasov@scavasoft.com>
2021-04-20 07:33:56 +0300

    prepare for practice classes

--- views/site/html/movieView.php
+++ views/site/html/movieView.php
@@ -10,18 +10,7 @@ echo "
 <form action='" . APPLICATION_PATH . "index.php?controller=movies&action=update&movie_id=$data->id' method='post'>
     <button type='submit'>Update</button>
 </form>
-<h3>Comments:</h3>
-<input id='message' type='text' placeholder='I like the movie...'>
-<button id='addComment' type='submit'>Comment</button>
-<p>Dilyan Atanasov: I like Borat as well 12/04/2021</p>
 ";
 
 echo "
-<script type='application/javascript'>
-const buttonForComment = document.getElementById('addComment');
-buttonForComment.onclick = () => {
-    let message = document.getElementById('message').value;
-    document.getElementById('message').value = '';
-}
-</script>
 ";
